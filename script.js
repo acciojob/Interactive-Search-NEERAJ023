7d763201e74a0918f8883abd6d5c7c294e076a79
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-17 23:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function activateSearch() {
+        var searchContainer = document.getElementById('searchContainer');
+        searchContainer.classList.add('active');
+
+        // Focus on the input element after activating
+        var inputElement = searchContainer.querySelector('.input');
+        inputElement.focus();
+    }
